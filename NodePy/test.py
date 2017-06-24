fa00f5fefd09afa3e3064e40073b8edf532ccdd9
esraa-saleh <esraa0x61@gmail.com>
2017-06-24 15:36:01 -0400

    Communications Basics done

--- NodePy/test.py
+++ NodePy/test.py
@@ -1,1 +1,10 @@
-print ("What does the fox say?");
+import serial
+import time
+#Confirms the test file opened
+print ("What does the fox say?")
+
+
+ser = serial.Serial('COM3',9600)
+#Send Start Signal that will prompt the action
+time.sleep(10)
+rtn = ser.write(1)
